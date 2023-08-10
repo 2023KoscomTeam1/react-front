e70a9260d228524fe2fcffe60bcb2aaa5c968304
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-10 23:10:38 +0900

    feat: Login router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import Login from "./routes/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/routes/Login.js
@@ -0,0 +1,4 @@
+function Login() {
+  return <div>Here goes login page</div>;
+}
+export default Login;
