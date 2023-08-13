10017c75f05ba2971bfb190a1eaf0ad85ed9b25a
pupafafa <wishluz1997@gmail.com>
2023-08-13 23:10:38 +0900

    🍱add : 신규 폰트 추가 및 반영

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -30,7 +30,7 @@ function Home() {
         {/* 배너 컨테이너 */}
         <div className="banner-container">
 
-          <span className="banner-title">현재 공모진행중</span>
+          <span className="banner-title">진행중인 공모</span>
           <div className="banner-wrapper">
             {/* 여러 개의 배너 이미지를 반복하여 표시 */}
             <img src="/img/banner1.png" alt="Banner 1" className="banner-image" />
