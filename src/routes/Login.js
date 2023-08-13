9c57dc57f6d20015b107f3b66baa82585382fbb0
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-13 10:42:34 +0900

    feat: api fetch

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,24 @@
 import { Chip } from "@mui/material";
+import ColorButton from "../components/Button";
+import { useEffect } from "react";
 
 function Login() {
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+    };
+    fetch("http://localhost:8080/user/root/place")
+      .then((response) => console.log(response.json()))
+
+      .catch((error) => console.log("error".error));
+  }, []);
+
   return (
     <div>
-      <div>Here goes login page</div>
+      <h3>로그인</h3>
+      <h3>이메일</h3>
+      <h3>비밀번호</h3>
+      <ColorButton text={"로그인"} size={20} />
       <Chip label="Chip Filled" />
     </div>
   );
