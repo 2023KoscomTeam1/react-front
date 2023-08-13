aea54f63f0b3ac4f8e30e50afebf0971ac41dc74
pupafafa <wishluz1997@gmail.com>
2023-08-13 22:53:36 +0900

    💄style : Home 화면  UI 개선

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,36 @@
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
 import React from "react";
 import "./Home.css";
 import ColorButton from "../components/Button";
 import Nav from "../components/Nav";
+import Button from "@mui/joy/Button";
+import EPortfolio from "../components/enterprise/Portfolio";
+
 function Home() {
   return (
     <div className="default-frame">
-      
+
       <div className="company-container">
         <Nav />
         <img src="/img/home/001.png" alt="home 1" className="home-image" />
         <img src="/img/home/intro_bold.png" alt="home 5" className="home-image" />
         <img src="/img/home/002.png" alt="home 3" className="home-image" />
-        <img src="/img/home/home_txt.png" alt="home 5" className="home-image" />
-        <img src="/img/home/004.png" alt="home 3" className="home-image" />
-        
         {/* "내 자산 등록하기" 버튼 */}
         <div className="register-button">
           <Link to="/company/portfolio">
-            <ColorButton text={"내 자산 등록하기"} size={25} />
+            <ColorButton text={"내 자산 등록하기"} size={5} />
           </Link>
         </div>
-        
-        <hr/>
+        <img src="/img/home/home_txt.png" alt="home 5" className="home-image" />
+        <img src="/img/home/004.png" alt="home 3" className="home-image" />
+
+
+        <hr />
         {/* 배너 컨테이너 */}
         <div className="banner-container">
+
+          <span className="banner-title">현재 공모진행중</span>
           <div className="banner-wrapper">
             {/* 여러 개의 배너 이미지를 반복하여 표시 */}
             <img src="/img/banner1.png" alt="Banner 1" className="banner-image" />
@@ -35,7 +41,7 @@ function Home() {
           </div>
         </div>
 
-        
+
 
       </div>
     </div>
