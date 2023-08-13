5c5b3f31f083bf70b9b82804c4eb95528c0f86d0
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-13 13:35:08 +0900

    feat: login formData

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,28 +1,72 @@
-import { Chip, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
 import ColorButton from "../components/Button";
 import "../../src/App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Nav from "../components/Nav";
 
 function Login() {
-  useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-    };
-    fetch("http://localhost:8080/user/root/place")
-      .then((response) => console.log(response.json()))
+  const [id, setId] = useState();
+  const [pwd, setPwd] = useState();
 
-      .catch((error) => console.log("error".error));
-  }, []);
+  /* item fetch 시 이하 코드 사용 */
+  // const requestOptions = async () => {
+  //   const json = await (
+  //     await fetch("http://localhost:8080/user/root/place")
+  //   ).json();
+  //   console.log(json);
+  // };
+  // useEffect(() => {
+  //   requestOptions();
+  // }, []);
+
+  const idHandler = (e) => {
+    setId(e.target.value);
+    console.log(id);
+  };
+
+  const pwdHandler = (e) => {
+    setPwd(e.target.value);
+    console.log(pwd);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("loginId", id);
+    formData.append("password", pwd);
+
+    const res = await fetch("http://localhost:8080/api/login", {
+      method: "POST",
+      body: formData,
+      mode: "no-cors",
+    });
+    console.log("this is res", res.ok);
+  };
 
   return (
-    <div className="login">
-      <h3>로그인</h3>
-      <h5>이메일</h5>
-      <TextField id="outlined-basic" variant="outlined" size="small" />
-      <h5>비밀번호</h5>
-      <TextField id="outlined-basic" variant="outlined" size="small" />
-      <br />
-      <ColorButton text={"로그인"} size={10} w={200} />
+    <div>
+      <Nav />
+      <form className="login" onSubmit={submitHandler}>
+        <label>로그인</label>
+        <h5>아이디</h5>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          size="small"
+          onChange={idHandler}
+        />
+        <h5>비밀번호</h5>
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          size="small"
+          onChange={pwdHandler}
+        />
+        <br />
+        <ColorButton text={"로그인"} size={10} w={200} t={"submit"} />
+      </form>
     </div>
   );
 }
