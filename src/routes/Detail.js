2ef59bb3c9f8610b97ac6d33e980b10e7700d07f
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-14 10:19:44 +0900

    feat: stockCount fetcher

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,12 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Nav from "../components/Nav";
+import ImageCropper from "../utils/ImageCropper";
+import { Image } from "@mui/icons-material";
+import { Container, Grid, Typography } from "@mui/material";
+import "./Home.css";
+
 function Detail({
   asset_id,
   name,
@@ -10,18 +15,47 @@ function Detail({
   whole_price,
   unit_current_price,
   end_price,
+  user_id,
 }) {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
   const [assets, setAssets] = useState({});
-  const getAssets = async () => {
-    const data = await axios.get(`http://localhost:8080/assets/${id}`);
-    setAssets(data.data.asset);
+  const [stockCount, setStockCount] = useState({});
+  const [buyOrderBook, setBuyOrderBook] = useState({});
+  const [sellOrderBook, setSellOrderBook] = useState({});
+  const getAssets = useCallback(async () => {
+    const { data } = await axios.get(`http://localhost:8080/assets/${id}`);
+    setAssets(data.asset);
     setLoading(false);
-  };
+  }, []);
+
+  const getStockCount = useCallback(async () => {
+    const { data } = await axios.get(
+      // TODO: Detail 활용할 때 user_id 받아오도록 하며 바로 아래의 내용으로 변경해야 함
+      // `http://localhost:8080/${user_id}/assets/${id}`
+      `http://localhost:8080/user/dechoi/assets/${id}`
+    );
+
+    setStockCount(data.user_assets[0].count);
+  }, []);
+
+  const getbuyOrderBook = useCallback(async () => {}, []);
+
+  const getsellOrderBook = useCallback(async () => {}, []);
+
   useEffect(() => {
     getAssets();
-    console.log(assets);
+  }, []);
+
+  useEffect(() => {
+    getStockCount();
+  }, []);
+
+  useEffect(() => {
+    getStockCount();
+  }, []);
+  useEffect(() => {
+    getStockCount();
   }, []);
 
   return (
@@ -33,12 +67,40 @@ function Detail({
         <h1>Loading</h1>
       ) : (
         <div className="default-frame">
-          <img src={assets.imageUrl} alt="assetId" className="home-image" />
+          {/* 짧은 것과 긴 것 비교해서 하기  */}
+          <div className="grid-container">
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="flex-start"
+              backgroundColor="red"
+              height="30"
+            >
+              hi
+            </Grid>
+          </div>
+
+          <img
+            src={assets.imageUrl}
+            alt="assetId"
+            className="home-image"
+            width="318"
+          />
+
           <h3>차트</h3>
-          <h3>호가</h3>
-          <h3>거래 정보</h3>
-          <h3>주문 수량</h3>
-          <h3>주문 가격</h3>
+          <div>{assets.name}</div>
+          <div>{assets.currnetUnitPrice}</div>
+          <div>{assets.address}</div>
+          <div>{stockCount} 잔고</div>
+          <div>차트</div>
+          <div>호가</div>
+          {/* 오늘자 외인, 개인, 기관 매수 혹은 매도 금액 표기 */}
+          <div>거래 정보</div>
+          <div>주문 수량</div>
+          <div>주문 가격</div>
+          <div>매수</div>
+          <div>매도</div>
         </div>
       )}
     </div>
