5c562d19b53f8850326bc122b8cee5ffbbb88698
Mingzhi <alswl_1110@naver.com>
2023-08-15 03:50:42 +0900

    ✏️Edit : ipo 디테일, 수정중

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./routes/Login";
 import List from "./routes/List";
 import MyPage from "./routes/MyPage";
 import Detail from "./routes/Detail";
+import IPODetail from "./routes/IPODetail";
 import Issues from "./routes/Issues";
 import EPortfolio from "./components/enterprise/Portfolio";
 import BasicTable from "./components/BasicTable";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/assets" element={<List />} />
         <Route path="/assets/detail/:id" element={<Detail />} />
+        <Route path="/ipo/detail/:id" element={<IPODetail />} />
         <Route path="/user/:id" element={<MyPage />} />
         <Route path="/issues" element={<Issues />} />
         <Route path="/company/portfolio" element={<EPortfolio />} />
