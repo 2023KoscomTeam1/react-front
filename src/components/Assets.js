470f6e66e03733f47f169b3b6bb5010930fa2cde
Mingzhi <alswl_1110@naver.com>
2023-08-14 18:59:37 +0900

    ✏️Edit : Asset List 수정

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -1,28 +1,48 @@
-import { Link } from "react-router-dom";
-import "../App.css";
+import { useNavigate } from "react-router-dom";
+import "./Assets.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { IoLocationSharp } from "react-icons/io5";
 
 function Assets({
-  asset_id,
-  name,
-  image_url,
-  address,
-  whole_price,
-  unit_current_price,
-  end_price,
-  count,
-  averagePrice,
+  asset
 }) {
+  const navigate = useNavigate();
+  const navigateAssetDetail = (asset_id) => {
+    // 👇️ navigate to /
+    navigate(`/assets/detail/${asset_id}`);
+  };
+  const change = asset.currentUnitPrice - asset.endPrice; 
+  const change_percent = (change / asset.endPrice) * 100;
+  const str_change = change ? change.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : change;
+  const str_wholePrice = asset.wholePrice ? (asset.wholePrice).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : asset.wholePrice;
+  const str_currentUnitPrice = asset.currentUnitPrice ? (asset.currentUnitPrice).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : asset.currentUnitPrice;
   return (
-    <div className="asset-box">
-      <img className="asset-preview" src={image_url} alt={name} />
-      <div className="inblock-item">{asset_id}</div>
-      {/* <Link to={`/assets/detail/${asset_id}`}>{name}</Link>
-      <div>{address}</div>
-      <div>{whole_price}</div>
-      <div>{unit_current_price}</div>
-      <div>{end_price}</div> */}
-      <div className="inblock-item"> 보유수량: {count}</div>
-      <div className="inblock-item"> 평균가: {averagePrice}</div>
+    <div className="asset-box" onClick={() => navigateAssetDetail(asset.assetId)}>
+      <div className="img-wrapper">
+        <img src={asset.imageUrl} alt={asset.name}/>
+      </div>
+      <div>
+        <div className="asset-topbox">
+            <div className="info-span">
+                <div className="asset-name">{asset.name}</div>
+                <div className="basic-text"> 평가금액: {str_wholePrice}</div>
+            </div>
+
+            <div className="price-span">
+                <div className="amount-large">{str_currentUnitPrice}</div>
+                <div className={`${change > 0 ? "positive" : change < 0 ? "negative" : "neutral"}`}>
+                {change > 0 ? "▲" : change < 0 ? "▼" : ""} 
+                {str_change}
+                ({Math.round(change_percent * 100)/100}%)</div>
+            </div>
+        </div>
+
+        <div className="location-label">
+            <IoLocationSharp/>
+            <div className="address-label">{asset.address}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/routes/List.js
+++ src/routes/List.js
@@ -2,40 +2,22 @@ import { useEffect, useState } from "react";
 import Assets from "../components/Assets";
 import Nav from "../components/Nav";
 import "../App.css";
+import axios from "axios";
 import SearchBox from "../components/SearchBox";
 
 function List() {
   const [loading, setLoading] = useState(true);
   const [assets, setAssets] = useState();
   const getAssets = async () => {
-    const json = [
-      {
-        asset_id: "1234",
-        name: "fake_asset1",
-        address: "fake_address1",
-        whole_price: 1234,
-        current_price: 4321,
-        company_own_count: 1,
-        foreign_own_count: 1,
-        personal_own_count: 1,
-      },
-      {
-        asset_id: "1235",
-        name: "fake_asset2",
-        address: "fake_address2",
-        whole_price: 1235,
-        current_price: 5321,
-        company_own_count: 2,
-        foreign_own_count: 2,
-        personal_own_count: 2,
-      },
-    ];
-    setAssets(json);
-    setLoading(false);
+    const res = await axios.get(`http://localhost:8080/assets/list`)
+      console.log('this is assets info', res.data.assets);
+      setAssets(res.data.assets);
+      setLoading(false);
   };
   useEffect(() => {
     getAssets();
   }, []);
+
 
   return (
     <div>
@@ -47,24 +29,18 @@ function List() {
         <div>
           <SearchBox text="투자 상품 검색" w={300}></SearchBox>
           <div className="tab-buttons">
-          <button className="tab-title-btn">전체 매물</button>
-          <button className="tab-title-btn">인기 매물</button>
-          <button className="tab-title-btn">나의 지역</button>
-          <button className="tab-title-btn">공모 중</button>
+          <button className="tab-title-btn" >전체 매물</button>
+          <button className="tab-title-btn" >인기 매물</button>
+          <button className="tab-title-btn" >나의 지역</button>
+          <button className="tab-title-btn" >공모 중</button>
           </div>
-          {assets.map((asset) => (
-            <div className="color-under-bar"><Assets
-              key={asset.asset_id}
-              asset_id={asset.asset_id}
-              name={asset.name}
-              image_url={asset.image_url}
-              address={asset.address}
-              whole_price={asset.whole_price}
-              unit_current_price={asset.unit_current_price}
-              end_price={asset.end_price}
-            />
-            </div>
-          ))}
+          <hr/>
+          
+            {assets.map((asset) => (
+              <div className="color-under-bar">
+                <Assets asset={asset}/>
+              </div>
+            ))}
         </div>
       )}
       </div>
