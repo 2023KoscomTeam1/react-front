340521c6f1c5a22a95ef564966ad49dead2304aa
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-15 14:11:39 +0900

    feat: DetailView detection

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,6 +10,7 @@ import BasicTable from "../components/BasicTable";
 import { Button, Sheet } from "@mui/joy";
 import InfoData from "../components/InfoData";
 import ViewPDF from "../components/individual/GovermentPDF";
+import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
 
 function createData(sell, orderPrice, buy) {
   return { sell: sell ? sell : 0, orderPrice, buy: buy ? buy : 0 };
@@ -30,31 +31,37 @@ function Detail({
   const [loading, setLoading] = useState(true);
   const [fetching, setFetching] = useState(true);
   const [assets, setAssets] = useState({});
-  const [stockCount, setStockCount] = useState({});
+  const [stockCount, setStockCount] = useState(0);
   const [buyOrderBook, setBuyOrderBook] = useState({});
   const [sellOrderBook, setSellOrderBook] = useState({});
   const [orderRange, setOrderRange] = useState({});
   const [activeInfo, setActiveInfo] = useState(null); // 현재 활성화된 화면을 상태로 관리
-
+  const [viewer, setViewer] = useState("");
+  const auth = useAuthUser();
+  const isAuthenticated = useIsAuthenticated();
   const handleButtonClick = (screen) => {
     setActiveInfo(screen); // 버튼 클릭에 따라 화면 변경
   };
 
   const getAssets = useCallback(async () => {
-    const { data } = await axios.get(`http://localhost:8080/assets/${id}`);
-    setAssets(data.asset);
-    setLoading(false);
+    try {
+      const { data } = await axios.get(`http://localhost:8080/assets/${id}`);
+      setAssets(data.asset);
+      setLoading(false);
+    } catch (e) {}
   }, []);
 
   const getStockCount = useCallback(async () => {
-    const { data } = await axios.get(
-      // TODO: Detail 활용할 때 user_id 받아오도록 하며 바로 아래의 내용으로 변경해야 함
-      // `http://localhost:8080/${user_id}/assets/${id}`
-      `http://localhost:8080/user/dechoi/assets/${id}`
-    );
+    try {
+      const { data } = await axios.get(
+        // TODO: Detail 활용할 때 user_id 받아오도록 하며 바로 아래의 내용으로 변경해야 함
+        // `http://localhost:8080/${user_id}/assets/${id}`
+        `http://localhost:8080/user/${viewer}/assets/${id}`
+      );
 
-    setStockCount(data.user_assets[0].count);
-  }, []);
+      setStockCount(data.user_assets[0].count);
+    } catch (e) {}
+  }, [viewer, id]);
 
   const getbuyOrderBook = useCallback(async () => {
     const { data } = await axios.get(`http://localhost:3000/order/${id}/sell`);
@@ -71,14 +78,17 @@ function Detail({
     setBuyOrderBook(sellOrder);
     setFetching(true);
   }, []);
-
+  useEffect(() => {
+    isAuthenticated() && setViewer(Object.values(auth())[0]);
+    // console.log("this is detail auth", user);
+  });
   useEffect(() => {
     getAssets();
   }, []);
 
   useEffect(() => {
     getStockCount();
-  }, []);
+  }, [viewer]);
 
   useEffect(() => {
     getbuyOrderBook();
