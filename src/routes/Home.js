57810fa4b990ddada3a41c171a84091f0e5854d5
Mingzhi <alswl_1110@naver.com>
2023-08-15 20:08:44 +0900

    ✏️Edit : home button 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -13,12 +13,17 @@ function Home() {
       <div className="default-frame">
         <div className="company-container">
           <img src="/img/home/001.png" alt="home 1" className="home-image" />
+          <div className="register-button" style={{marginTop:"-100px"}}>
+            <Link to="/assets">
+              <ColorButton text={"투자하러하기"} size={5} />
+            </Link>
+          </div>
           <img src="/img/home/intro_bold.png" alt="home 5" className="home-image" />
           <img src="/img/home/002.png" alt="home 3" className="home-image" />
           {/* "내 자산 등록하기" 버튼 */}
           <div className="register-button">
             <Link to="/company/portfolio">
-              <ColorButton text={"내 자산 등록하기"} size={5} />
+              <ColorButton text={"내 자산 확인하기"} size={5} />
             </Link>
           </div>
           <img src="/img/home/home_txt.png" alt="home 5" className="home-image" />
