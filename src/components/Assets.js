455e2d208e3a9239396e60c0ac1008a93a6067c1
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2023-08-15 19:02:27 +0900

    feat: popular, hometown assets

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -4,43 +4,60 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { IoLocationSharp } from "react-icons/io5";
 
-function Assets({
-  asset
-}) {
+function Assets({ asset }) {
   const navigate = useNavigate();
   const navigateAssetDetail = (asset_id) => {
     // 👇️ navigate to /
     navigate(`/assets/detail/${asset_id}`);
   };
-  const change = asset.currentUnitPrice - asset.endPrice; 
+  const change = asset.currentUnitPrice - asset.endPrice;
   const change_percent = (change / asset.endPrice) * 100;
-  const str_change = change ? change.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : change;
-  const str_wholePrice = asset.wholePrice ? (asset.wholePrice).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : asset.wholePrice;
-  const str_currentUnitPrice = asset.currentUnitPrice ? (asset.currentUnitPrice).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") : asset.currentUnitPrice;
+  const str_change = change
+    ? change.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    : change;
+  const str_wholePrice = asset.wholePrice
+    ? asset.wholePrice
+        .toString()
+        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    : asset.wholePrice;
+  const str_currentUnitPrice = asset.currentUnitPrice
+    ? asset.currentUnitPrice
+        .toString()
+        .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")
+    : asset.currentUnitPrice;
+
+  useEffect(() => {}, [asset]);
   return (
-    <div className="asset-box" onClick={() => navigateAssetDetail(asset.assetId)}>
+    <div
+      className="asset-box"
+      onClick={() => navigateAssetDetail(asset.assetId)}
+    >
       <div className="img-wrapper">
-        <img src={asset.imageUrl} alt={asset.name}/>
+        <img src={asset.imageUrl} alt={asset.name} />
       </div>
       <div>
         <div className="asset-topbox">
-            <div className="info-span">
-                <div className="asset-name">{asset.name}</div>
-                <div className="basic-text"> 평가금액: {str_wholePrice}</div>
-            </div>
+          <div className="info-span">
+            <div className="asset-name">{asset.name}</div>
+            <div className="basic-text"> 평가금액: {str_wholePrice}</div>
+          </div>
 
-            <div className="price-span">
-                <div className="amount-large">{str_currentUnitPrice}</div>
-                <div className={`${change > 0 ? "positive" : change < 0 ? "negative" : "neutral"}`}>
-                {change > 0 ? "▲" : change < 0 ? "▼" : ""} 
-                {str_change}
-                ({Math.round(change_percent * 100)/100}%)</div>
+          <div className="price-span">
+            <div className="amount-large">{str_currentUnitPrice}</div>
+            <div
+              className={`${
+                change > 0 ? "positive" : change < 0 ? "negative" : "neutral"
+              }`}
+            >
+              {change > 0 ? "▲" : change < 0 ? "▼" : ""}
+              {str_change}({Math.round(change_percent * 100) / 100}%)
             </div>
+          </div>
         </div>
 
         <div className="location-label">
-            <IoLocationSharp/>
-            <div className="address-label">{asset.address}</div>
+          <IoLocationSharp />
+          <div className="address-label">{asset.address}</div>
         </div>
       </div>
     </div>
--- src/routes/List.js
+++ src/routes/List.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Assets from "../components/Assets";
 import Nav from "../components/Nav";
 import "../App.css";
@@ -10,29 +10,105 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import axios from "axios";
 import SearchBox from "../components/SearchBox";
+import BasicTable from "../components/BasicTable";
+import { InputBase } from "@mui/material";
+import { useAuthUser, useIsAuthenticated } from "react-auth-kit";
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(2),
+  },
+  "& .MuiInputBase-input": {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)",
+    },
+  },
+}));
 
 function List() {
-  const [activeInfo, setActiveInfo] = useState(null); // 현재 활성화된 화면을 상태로 관리
+  const [activeInfo, setActiveInfo] = useState("all"); // 현재 활성화된 화면을 상태로 관리
 
+  const [loading, setLoading] = useState(true);
+  const [assets, setAssets] = useState();
+  const [popular, setPopular] = useState();
+  const [home, setHome] = useState();
+
+  const [viewer, setViewer] = useState();
+
+  const auth = useAuthUser();
+  const isAuthenticated = useIsAuthenticated();
   const handleButtonClick = (screen) => {
     setActiveInfo(screen); // 버튼 클릭에 따라 화면 변경
   };
-  const [loading, setLoading] = useState(true);
-  const [assets, setAssets] = useState();
-  const getAssets = async () => {
+  const getAssets = useCallback(async () => {
     const res = await axios.get(`http://localhost:8080/assets/list`);
     console.log("this is assets info", res.data.assets);
     setAssets(res.data.assets);
     setLoading(false);
-  };
-  useEffect(() => {
-    getAssets();
-  }, []);
+  }, [assets]);
+
+  const getPopular = useCallback(async () => {
+    const newAssets = assets.filter((asset) => asset.viewCount % 2 == 1);
+    setPopular(newAssets);
+  }, [assets, popular]);
+
+  const getHome = useCallback(async () => {
+    console.log("im in get home");
+    const res = await axios.get(`http://localhost:8080/user/${viewer}/place`);
+    const homeTown = res.data.user_place;
+    const myHome = assets.filter((asset) => asset.placeType === homeTown);
+    setHome(myHome);
+  }, [viewer, assets]);
 
   const place = "경기";
   const handleChange = () => {
     return;
   };
+
+  useEffect(() => {
+    isAuthenticated() && setViewer(Object.values(auth())[0]);
+  });
+
+  useEffect(() => {
+    getAssets();
+    console.log("activeInfo:", activeInfo);
+  }, [activeInfo]);
+
+  useEffect(() => {
+    assets && getPopular();
+    assets && console.log("this is popular2", popular);
+  }, [assets]);
+
+  useEffect(() => {
+    viewer && assets && getHome();
+    console.log("this is viewer", viewer);
+    console.log("this is viewer", home);
+  }, [viewer, assets]);
+
+  useEffect(() => {
+    console.log("this is home", home);
+  }, [home]);
 
   return (
     <div>
@@ -43,7 +119,6 @@ function List() {
         ) : (
           <div>
             <SearchBox text="투자 상품 검색" w={300}></SearchBox>
-
             <span className="button-container">
               <ButtonGroup aria-label="outlined primary button group">
                 <Button
@@ -82,16 +157,17 @@ function List() {
               <button className="tab-title-btn" >나의 지역</button>
               <button className="tab-title-btn" >공모 중</button>
             </div> */}
+            <br />
             <div className="place-combo">
               <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-                <InputLabel id="demo-simple-select-label">Place</InputLabel>
+                {/* <InputLabel id="demo-customized-select-label">장소</InputLabel> */}
                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
+                  labelId="demo-customized-select-label"
+                  id="demo-customized-select"
                   value={place}
-                  label="Age"
                   onChange={handleChange}
-                  style={{}}
+                  input={<BootstrapInput />}
+                  style={{ height: "30px" }}
                 >
                   <MenuItem value={"SEOUL"}>서울</MenuItem>
                   <MenuItem value={"KANGWON"}>강원도</MenuItem>
@@ -109,11 +185,28 @@ function List() {
                 </Select>
               </FormControl>
             </div>
-            {assets.map((asset) => (
-              <div className="color-under-bar">
-                <Assets asset={asset} />
-              </div>
-            ))}
+            <br />
+            {/* {activeInfo === "all" ? ( */}
+            {(activeInfo === "all" || (activeInfo === "hometown" && !viewer)) &&
+              assets.map((asset) => (
+                <div className="color-under-bar">
+                  <Assets asset={asset} />
+                </div>
+              ))}
+            {activeInfo === "popular" &&
+              popular.map((p) => (
+                <div className="color-under-bar">
+                  <Assets asset={p} />
+                </div>
+              ))}
+            {activeInfo === "hometown" &&
+              viewer &&
+              home &&
+              home.map((h) => (
+                <div className="color-under-bar">
+                  <Assets asset={h} />
+                </div>
+              ))}
           </div>
         )}
       </div>
